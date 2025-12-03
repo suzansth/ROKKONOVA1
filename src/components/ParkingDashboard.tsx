6cfc66c73dc054f967ec5d82e6958b2ea1272330
suzansth <pujanstha89@gmail.com>
2025-12-03 11:42:05 +0900

    Updated ParkingDashboard.tsx

--- src/components/ParkingDashboard.tsx
+++ src/components/ParkingDashboard.tsx
@@ -240,8 +240,8 @@ const ParkingDashboard: React.FC<ParkingDashboardProps> = ({
               <PieChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
                 <Pie
                   data={regionPieData}
-                  cx="100%"
-                  cy="100%"
+                  cx="60%"
+                  cy="60%"
                   labelLine={true}
                   label={({ name, percent }) => `${name}\n${(percent * 100).toFixed(1)}%`}
                   outerRadius="80%"
