86fa304aad5d71a38e25bf7576d2f3a67e11cc7c
suzansth <pujanstha89@gmail.com>
2025-12-09 13:26:59 +0900

    Updated ParkingDashboard.tsx

--- src/components/ParkingDashboard.tsx
+++ src/components/ParkingDashboard.tsx
@@ -237,7 +237,7 @@ const ParkingDashboard: React.FC<ParkingDashboardProps> = ({
   <h3 className="text-lg font-semibold text-gray-900 mb-6">用途別構成比</h3>
 
   <div className="flex justify-center">
-    <div className="w-[420px] h-[420px]">
+    <div className="w-[320px] h-[320px]">
       <PieChart width={320} height={320}>
         <Pie
           data={usagePieData}
